3b321b6a37cf142d57dfb9af17cf0d3547edc089
Murali Mohana Krishna Dandu <murali.munna94@gmail.com>
2021-04-30 00:07:16 -0700

    Added indexer and fullranker base structure

--- pipeline/msmarco_indexer.py
+++ pipeline/msmarco_indexer.py
@@ -1,1 +1,42 @@
-#This file indexes the ms marco pipeline
+"""
+Script to index MAMARCO documents
+"""
+import argparse
+import os
+
+import yaml
+
+from forte.common.configuration import Config
+from forte.data.data_pack import DataPack
+from forte.pipeline import Pipeline
+from forte.processors.ir import ElasticSearchPackIndexProcessor
+# from composable_source.readers import CORDReader
+from forte.data.readers import MSMarcoPassageReader
+
+
+def main(dataset_dir: str):
+    """
+    Build a pipeline to process MS_MARCO dataset using
+    MSMarcoPassageReader and build elastic indexer.
+    """
+    config_file = os.path.join(os.path.dirname(__file__), 'config.yml')
+    config = yaml.safe_load(open(config_file, "r"))
+    config = Config(config, default_hparams=None)
+
+    pipeline = Pipeline[DataPack]()
+
+    pipeline.set_reader(MSMarcoPassageReader())
+    pipeline.add(ElasticSearchPackIndexProcessor(), config=config.create_index)
+
+    pipeline.run(dataset_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir", type=str,
+                    default="data/collectionandqueries/",
+                    help="Data directory to read the collections tsv file from")
+
+    args = parser.parse_args()
+    main(args.data_dir)
+
--- readers/msmarco_reader.py
+++ readers/msmarco_reader.py
@@ -1,1 +1,44 @@
-#This code is to read msmarco reader
+"""
+Script for MSMARCO Reader.
+"""
+import argparse
+
+from forte.data.data_pack import DataPack
+from forte.pipeline import Pipeline
+from forte.processors.writers import PackNameJsonPackWriter
+
+# from composable_source.readers import CORDReader
+from forte.data.readers import MSMarcoPassageReader
+
+def main(dataset_dir: str, output_dir: str):
+    """
+    Build an NLP pipeline to process CORD_NER dataset using
+    CORDReader, then write the processed dataset out.
+    """
+    pipeline = Pipeline[DataPack]()
+    pipeline.set_reader(MSMarcoPassageReader())
+
+    pipeline.add(
+        PackNameJsonPackWriter(),
+        {
+            'output_dir': output_dir,
+            'indent': 2,
+            'overwrite': True,
+            'drop_record': True
+        }
+    )
+    pipeline.run(dataset_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-dir", type=str,
+                    default="sample_data/cord_paper/",
+                    help="Data directory to read the text files from.")
+    parser.add_argument("--output-dir", type=str,
+                    default='./',
+                    help="Output dir to save the processed datapack.")
+    args = parser.parse_args()
+    main(args.data_dir, args.output_dir)
+
+
