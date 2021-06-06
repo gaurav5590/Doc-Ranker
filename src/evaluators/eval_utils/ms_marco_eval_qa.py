ae1eb2a7680f4647ab8dd33a018a3ad4101dfe7d
gaurav5590 <gk59052@gmail.com>
2021-06-06 00:10:54 -0400

    Added F1 metric

--- src/evaluators/eval_utils/ms_marco_eval_qa.py
+++ src/evaluators/eval_utils/ms_marco_eval_qa.py
@@ -51,14 +51,6 @@ def compute_f1(a_gold, a_pred):
     pred_toks = get_tokens(a_pred)
     common = collections.Counter(gold_toks) & collections.Counter(pred_toks)
     num_same = sum(common.values())
-    if len(gold_toks) == 0 or len(pred_toks) == 0:
-        # If either is no-answer, then F1 is 1 if they agree, 0 otherwise
-        return int(gold_toks == pred_toks)
-    if num_same == 0:
-        return 0
-    # precision = 1.0 * num_same / len(pred_toks)
-    # recall = 1.0 * num_same / len(gold_toks)
-    # f1 = (2 * precision * recall) / (precision + recall)
     return num_same, len(pred_toks), len(gold_toks)
 
 def find_overall_f1(reference_dictionary, candidate_dictionary):
